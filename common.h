a01903ae549d79e21f232e9b9519deb35654b5b6
12baby21 <18917131713@163.com>
2021-12-30 08:04:37 -0500

    lll

--- common.h
+++ common.h
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <gmpxx.h>
-
+111
 void GenKey(mpz_ptr n, mpz_ptr g, mpz_ptr lambda, mpz_ptr mu, mp_bitcnt_t bits);
 void Encryption(mpz_ptr c, mpz_ptr m, mpz_ptr n, mpz_ptr g, mpz_ptr n_2);
 void Decryption(mpz_ptr m, mpz_ptr c, mpz_ptr lambda, mpz_ptr n_2, mpz_ptr mu, mpz_ptr n);
